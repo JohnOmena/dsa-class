41195c8dfe485f3273fa0d57977d1710ae30a58c
John Omena <jvfo@ic.ufal.br>
2022-02-10 20:11:19 -0300

    Create utils-functions.R
    Adicionando árvore de pastas e código inicial em R

---
+++ data-science/r-language/utils-functions.R
@@ -0,0 +1,49 @@
+# Funcoes utilitarias
+
+# obter informacoes sobre os contribuidores
+contributors()
+
+# Obter informações da licenca
+license()
+
+# Obtem o diretorio de trabalho atual
+getwd()
+
+# Seta o diretorio de trabalho de acordo com o caminho passado como parametro
+setwd()
+
+# Obtem informacoes sobre a sessao
+sessionInfo()
+
+# Imprimir na tela
+print('R - Uma das principais ferramentas do Cientista de Dados')
+
+# Para instalar pacotes em R
+install.packages('randomForest')
+
+# Para Carregar o pacote em seu ambiente de trabalho
+library(randomForest)
+
+# Para descarregar um pacote
+detach(package:randomForest)
+
+# Pesquisar sobre uma funcao em especifico
+help(mean)
+?mean
+
+# Se quiser uma demonstracao de uso da funcao
+demon(mean)
+
+# Se nao souber o nome da funcao
+help.search('randomForest')
+??matplot
+RSiteSearch('matplot')
+example('matplot')
+
+# Executa comandos existentes dentro de um outro script
+source()
+
+# Sair
+q()
+
+
